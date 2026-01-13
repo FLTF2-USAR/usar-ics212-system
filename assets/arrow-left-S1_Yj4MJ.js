5a12dc31fc5da2fe705b9e31d1df47d98124b01f
FLTF2-USAR <FLTF2-USAR@users.noreply.github.com>
2026-01-13 11:20:24 +0000

    deploy: 567d985a2d695f615ab86f2207d000b48d3679fe

---
+++ assets/arrow-left-S1_Yj4MJ.js
@@ -0,0 +1,2 @@
+import{c as o}from"./index-C0m64t3w.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
+//# sourceMappingURL=arrow-left-S1_Yj4MJ.js.map
