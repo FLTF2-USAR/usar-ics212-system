06831f195d8285cfa69be0c5ab2a6a9dacd12e57
FLTF2-USAR <FLTF2-USAR@users.noreply.github.com>
2026-01-13 15:17:34 +0000

    deploy: 7eeb31bb8012280c5dc9f93e30852b0e8247df9f

---
+++ assets/circle-check-big-BR4FSz7p.js
@@ -0,0 +1,2 @@
+import{c}from"./index-ALhHeptV.js";const e=[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]],t=c("circle-check-big",e);export{t as C};
+//# sourceMappingURL=circle-check-big-BR4FSz7p.js.map
